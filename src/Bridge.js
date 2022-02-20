3e7b866cef3f3fadd94b8d5de2534353d8841ef4
violetflare@protonmail.com <violetflare@protonmail.com>
2022-02-20 19:25:12 +0100

    add things

--- src/Bridge.js
+++ src/Bridge.js
@@ -1,5 +1,55 @@
 class Bridge {
 
+    constructor(guild, DAL) {
+        this.prefix = "br";
+        this.guild = guild;
+        this.DAL = DAL;
+        this.configuration = {};
+    }
+
+    init() {
+        this.DAL.insertGuild(this.guild.id, this.guild.name);
+        this.DAL.getConfiguration(this.guild.id).then(config => {
+            if (config) {
+
+            }
+        });
+    }
+
+    _configure(channelName) {
+      const matchingChannel = this.guild.channels.cache.find(channel => channel.name === channelName);
+      this.DAL.updateConfiguration(matchingChannel);
+    }
+
+    _splitCommand(msg) {
+        const indexOfFirstSpaceOccurrence = msg.content.indexOf(" ");
+        const firstPartOfCommand = msg.content.substring(0, indexOfFirstSpaceOccurrence);
+        const lastPartOfCommand = msg.content.substring(indexOfFirstSpaceOccurrence + 1, msg.content.length);
+        const splittedCommand = [firstPartOfCommand, lastPartOfCommand];
+
+        return splittedCommand;
+    }
+
+    _parseCommand(msg) {
+        let splittedCommand = this._splitCommand(msg);
+        splittedCommand = splittedCommand.filter(string => string !== "" && string !== " ");
+        const prefix = splittedCommand[0] ? splittedCommand[0].toLowerCase() : "";
+        
+        if (prefix.includes(this.prefix)) {
+          const commandNameSplitted = splittedCommand[0].split("/");
+          const command = commandNameSplitted[1] ? commandNameSplitted[1].toLowerCase() : "";
+    
+          switch (command) {
+            case "configure":
+              this._configure(splittedCommand[1]);
+            break;
+          }
+        }
+      }
+
+    onMessageCreate(msg) {
+        this._parseCommand(msg);
+    }
 }
 
 module.exports = Bridge;
---
+++ src/DAL/DataLayer.js
@@ -0,0 +1,83 @@
+const DB = require('./DB.js');
+const mysql = require('mysql2');
+
+class DataLayer {
+
+    constructor() {
+
+    }
+
+    insertGuild(guildId, name) {
+        DB.getConnection((err, connection) => {
+            if (err) throw err;
+
+            const escapedName = mysql.escape(name);
+
+            const query = `
+                INSERT INTO bridge_guilds
+                    (id, name)
+                VALUES
+                    (${guildId}, ${escapedName})
+               ON DUPLICATE KEY UPDATE
+                    name = ${escapedName};
+            `
+
+            connection.query(query, (error, results, fields) => {
+                connection.release();
+                if (error) throw error;
+            });
+        });
+    }
+
+    updateConfiguration(channel) {
+        DB.getConnection((err, connection) => {
+            if (err) throw err;
+
+            const channelName = mysql.escape(channel.name);
+
+            const query = `
+                INSERT INTO bridge_configs
+                    (guild_id, channel_name, channel_id)
+                VALUES
+                    (${channel.guild.id}, ${channelName}, ${channel.id})
+                ON DUPLICATE KEY UPDATE
+                    channel_name = ${channelName},
+                    channel_id = ${channel.id};
+            `
+
+            connection.query(query, (error, results, fields) => {
+                connection.release();
+                if (error) throw error;
+            });
+        });
+    }
+
+    getConfiguration(guildId) {
+        return new Promise(
+            (resolve, reject) => {
+                DB.getConnection((err, connection) => {
+                    if (err) throw err;
+        
+                    const query = `
+                        SELECT * from bridge_configs
+                        WHERE guild_id = ${guildId};
+                    `
+        
+                    connection.query(query, (error, results, fields) => {
+                        if (error) throw error;
+                        connection.release();
+
+                        if (results === undefined) {
+                            reject(new Error("Results is undefined."))
+                        } else {
+                            resolve(results[0]);
+                        }
+                    });
+                });
+            }
+        );
+    }
+
+}
+
+module.exports = new DataLayer();
